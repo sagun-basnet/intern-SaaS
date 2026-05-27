f3d24cfea888480ebc3214d6b4045aa07f97d1eb
sagun basnet <basnetsagun8@gmail.com>
2026-05-27 17:18:03 +0545

    vercel config

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 require('dotenv').config();
 const app = require('./src/app');
-module.exports = app;
+
+// Vercel expects a function (req, res) => {}. Forward to the Express app.
+module.exports = (req, res) => app(req, res);
